83840ce5b648d230e261c5c388d9286106ea5558
Calvin Ho <18680207+calvh@users.noreply.github.com>
2020-12-10 23:07:49 -0500

    Add login and register page
    Add routes to render login and register
    Modify style

--- routes/render/index.js
+++ routes/render/index.js
@@ -2,6 +2,12 @@ module.exports = (router) => {
   router.get("/", (req, res) => {
     res.render("index");
   });
+  router.get("/login", (req, res) => {
+    res.render("login", { style: "signin.css" });
+  });
+  router.get("/register", (req, res) => {
+    res.render("register", { style: "signin.css" });
+  });
   router.get("/dashboard", (req, res) => {
     res.render("dashboard");
   });
