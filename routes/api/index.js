53ddf51d74fa3e6c10b6a38d06a2ae98ab746ebb
Calvin Ho <18680207+calvh@users.noreply.github.com>
2020-12-10 22:24:40 -0500

    Fix incorrect dependencies and route definitions
    Added missing return statement to subrouters

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
-module.exports = (router, passport, db, jwt) => {
+module.exports = (router, passport, jwt, db) => {
   /* GET users listing. */
   router.get("/users", function (req, res, next) {
     res.send("respond with a resource");
   });
+
+  return router;
 };
