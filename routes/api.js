eaabea51141098275685a59590cde09a566e05f6
Calvin Ho <18680207+calvh@users.noreply.github.com>
2020-12-24 21:57:06 -0500

    Add feature for chart to respond to data
    Refactor and add frontend code
    Add delete route to API routes
    Various frontend tweaks

--- routes/api.js
+++ routes/api.js
@@ -12,10 +12,28 @@ module.exports = (router, passport, jwt, db) => {
         return res.redirect("/login");
       }
 
-      db.Datapoint.find({ user: user._id })
-      .then((dbDatapoints) => res.json(dbDatapoints))
-      .catch((err) => res.status(500).send(err));
+      db.Datapoint.find({ user: user._id }, "date mood")
+        .then((dbDatapoints) => res.json(dbDatapoints))
+        .catch((err) => res.status(500).send(err));
+    })(req, res, next);
+  });
 
+  router.put("/data", (req, res, next) => {
+    passport.authenticate("jwtAccess", (err, user, info) => {
+      if (err) {
+        // 500 Internal Server Error: The server has encountered a situation it doesn't know how to handle
+        console.log(err);
+        return res.status(500).send(err);
+      }
+
+      if (!user) {
+        // invalid refresh token or user not found
+        return res.redirect("/login");
+      }
+
+      db.Datapoint.deleteMany({ user: user._id })
+        .then((result) => res.json(result))
+        .catch((err) => res.status(500).send(err));
     })(req, res, next);
   });
 
@@ -34,15 +52,14 @@ module.exports = (router, passport, jwt, db) => {
 
       /*
       req.body = {
-        entryDate: Date(),
-        mood: 5
+        entryDate: Date,
+        mood: int
       }
       */
 
       db.Datapoint.create({ user: user._id, ...req.body })
         .then((dbDatapoint) => res.json(dbDatapoint))
         .catch((err) => res.status(500).send(err));
-        
     })(req, res, next);
   });
 
